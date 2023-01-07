5cd471b4c12369ed219940a919c73a2d9461facb
yashj <yash.jain@augmentolabs.com>
2023-01-07 10:25:11 +0530

    updated test cases

--- FindStringLength/UnitTest1.cs
+++ FindStringLength/UnitTest1.cs
@@ -1,14 +1,26 @@
 using LearnConsoleApp;
+using Xunit;
+
 namespace FindStringLength
 {
     public class UnitTest1
     {
         [Fact]
-        public void checkLength()
+        public void testCases()
         {
             LengthHelper lh = new LengthHelper();
-            string example = "Test String";
-            Assert.Equal(example.Length,lh.findLength(example));
+
+            string testString1 = "";
+            string testString2 = "123";
+            string testString3 = "123456789010";
+            string testString4 = null;
+
+            Assert.Equal(0,lh.findLength(testString1));   // ""
+            Assert.Equal(3, lh.findLength(testString2));  // "123"
+            Assert.Equal(1, lh.findLength(testString3)); // "123456789010"
+            Assert.Equal(1, lh.findLength(testString4));  // null
+            
+
         }
     }
 }
